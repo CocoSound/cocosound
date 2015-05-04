e47f2563d041aa18d9938d881b57a2ee7ab04732
Ezerah <kilian.bellot@hotmail.fr>
2015-05-04 14:42:41 +0200

    Ajout de la page de résultat de recherche
    Recherche et affichage des résultats : fonctionnel

--- search_results.php
+++ search_results.php
@@ -12,6 +12,7 @@
     	<title>Coco Sound</title>
     	<meta name="viewport" content="width=device-width"/>
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" type="text/css" href="css/search_results.css" />
    </head>
    <body>
    		<div id="wrapper">
@@ -102,15 +103,61 @@
 			</header>
 			<div id="main">
 				<div id="container">
-					<form id="searchbar" method="get" name="search" action="search_results.php">
-						<input type="text" name ="searchkey" placeholder="Recherchez par..." required>
-						<select name="searchby">
-  							<option value=1>Artiste</option>
-  							<option value=2>Titre</option>
-  							<option value=3>Genre</option>
-						</select>
-						<button type="submit">Recherche</button>
-					</form>
+					<?php
+						if(isset($_GET['searchby']))
+						{
+							try 
+							{
+								$bdd = new PDO('mysql:host=localhost;dbname=cocosound;charset=utf8', 'root', '');
+							}
+							catch (Exception $e)
+							{
+							    die('Erreur : ' . $e->getMessage());
+							}
+
+							$searchby = $_GET['searchby'];
+							$key = $_GET['searchkey'];
+							if($searchby == 1)
+							{
+								$query = $bdd->prepare("SELECT * FROM musique WHERE Artiste LIKE '%".$key."%'");
+							}	
+							else if ($searchby == 2) 
+							{
+								$query = $bdd->prepare("SELECT * FROM musique WHERE Titre LIKE '%".$key."%'");
+							}
+							else
+							{
+								$query = $bdd->prepare("SELECT * FROM musique WHERE Genre LIKE '%".$key."%'");
+							}
+							
+							if (!empty($query))
+							{
+								$query->execute();
+								$data = $query->fetchAll();
+
+								if($searchby == 1) $printsearch = "Artiste";
+								if($searchby == 2) $printsearch = "Titre";
+								if($searchby == 3) $printsearch = "Genre";
+								echo('<div id="print_results">Résultats trouvés pour la recherche par <b>'.$printsearch.'</b></br>Mot clé : <b>'.$key.'</b></div>');
+								foreach($data as $value)
+								{
+								echo('
+									<div class="rightborder">
+										<div id="sound_container">
+											<div class="sound">
+												<div class="title">'.$value['Titre'].'</div>
+												<div class="artist">- <b>'.$value['Artiste'].'</b></div>
+												<div class="style">'.$value['Genre'].'</div>
+												<audio src="'.$value['Chemin_Musique'].'" controls></audio>
+											</div>
+											<div class="endline"></div>
+										</div>
+									</div>
+									');
+								}	
+							}
+						}
+					?>
 				</div>
 			</div>
 			<footer id="footer">
