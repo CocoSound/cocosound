065e31eb812d838a12c30f72cf769cae387a90c1
Aline - L <aline.legros80@gmail.com>
2015-05-05 20:47:40 +0200

    MAJ upload.php

--- upload.php
+++ upload.php
@@ -21,7 +21,7 @@
    	<body>
 			<?php include("header.php"); ?>
 		<form id ="upload" method="post" action="upload.php" enctype="multipart/form-data">
-			<div id="wrapper1"><input type="file" name="fichier" size="300000000000"></div>
+			<div id="wrapper1"><input type="file" name="fichier" size="30"></div>
 			<div id="soundinputs">
 				<aside .align><input type="text" name="Titre" placeholder="Titre"></aside>
 				<aside .align><input type="text" name="Artiste" placeholder="Artiste"></aside>
@@ -29,49 +29,71 @@
 			</div>
 			<div id="wrapper2"><input type="submit" name="upload" value="Upload"></div>
 		<?php
-			if( isset($_POST['upload']) ) // si formulaire soumis
-			{
-				include("connect.php");
-				$content_dir = './upload/'; // dossier où sera déplacé le fichier
-			    $emplacementTemporaire = $_FILES['fichier']['tmp_name'];
-
-			    if( !is_uploaded_file($emplacementTemporaire) )
-			    {
-			        echo('<p>Le fichier est introuvable</p>');
-			    }
-				else{
-						// on vérifie maintenant l'extension
-						$infosfichier = pathinfo($_FILES['fichier']['name']);
-						$type_file = $infosfichier['extension'];
-						$ext_autorisees= array('mp3','m4a','mp4','mpeg');
-						
-						if(!in_array($type_file,$ext_autorisees)){
-							echo('<p>Le fichier n\'est pas valide</p>');
+			if( isset($_POST['upload'])){ // si formulaire soumis
+					if(!empty($_POST['Titre']) && !empty($_POST['Artiste']) && !empty($_POST['Style'])){ 
+						if($_FILES['fichier']['error'] != 0){ 
+						echo('<p>Pas de fichier sélectionné</p>');
 						}
 						else{
-							// on copie le fichier dans le dossier de destination
-								$name_file = $_FILES['fichier']['name'];
-								
-								$query3 = $bdd->prepare('SELECT Titre, Artiste FROM musique WHERE Titre=? AND Artiste=?');
-								$query3->execute(array($_POST['Titre'],$_POST['Artiste']));
-								if(!(strtolower($_POST['Titre']) == strtolower($query3->fetchColumn(0)))&&(!(strtolower($_POST['Artiste'])==strtolower($query3->fetchColumn(1))))){
-									move_uploaded_file($emplacementTemporaire, $content_dir . basename($name_file));
-									echo('<p>Le fichier a bien été uploadé</p>');
-									$query5=$bdd->prepare('INSERT INTO musique(Artiste, Titre, Genre, Chemin_Musique) VALUES(:val1, :val2, :val3, :val4)');
-									$query5->execute(array('val1'=>$_POST['Artiste'], 'val2'=>$_POST['Titre'], 'val3'=>$_POST['Style'], 'val4'=>$content_dir.$name_file));
-								}
-								else{
-										echo('<p> Titre existant </p>');
+							if($_FILES['fichier']['size'] > 20971520){//20 Mo
+								echo('<p>La taille du fichier ne doit pas dépasser 20 Mo!</p>');
+							}
+							else{
+									include("connect.php");
+									//on vérifie l'extension du fichier
+									$infosfichier = pathinfo($_FILES['fichier']['name']);
+									$type_file = $infosfichier['extension'];
+									$ext_autorisees= array('mp3','m4a','mp4','mpeg');
+									if(!in_array($type_file,$ext_autorisees)){
+											echo('<p>Le fichier n\'est pas valide</p>');
+										}
+										else{	
+												//On teste si la musique existe dans la table "musique" de la BD
+												$titre=strtolower($_POST['Titre']);
+												$artiste=strtolower($_POST['Artiste']);
+												
+												$query3 = $bdd->prepare('SELECT COUNT(Numero_Musique) FROM musique WHERE Titre=? AND Artiste=?');
+												$query3->execute(array($titre,$artiste));
+												$resultat=$query3->fetch();
+												$nbMusiques = $resultat['COUNT(Numero_Musique)'];
+												$query3->closeCursor();
+												
+												//Cas où elle n'est pas dans la base :
+												if($nbMusiques==0){
+													// On met le fichier dans le répertoirec"upload" sur le serveur
+													$content_dir = './upload/';
+													$emplacementTemporaire = $_FILES['fichier']['tmp_name'];
+													$name_file = $_FILES['fichier']['name'];
+													move_uploaded_file($emplacementTemporaire, $content_dir . basename($name_file));
+													
+													// Et on ajoute une instance dans la base musique
+													$genre=strtolower($_POST['Style']);
+													$query4=$bdd->prepare('INSERT INTO musique(Artiste, Titre, Genre, Chemin_Musique) VALUES(:val1, :val2, :val3, :val4)');
+													$query4->execute(array('val1'=>$artiste, 'val2'=>$titre, 'val3'=>$genre, 'val4'=>$content_dir.$name_file));
+													
+												}
+												//Dans tous les cas :
+												// on récupère le numéro de la musique
+												$query5=$bdd->prepare('SELECT Numero_Musique FROM musique WHERE Titre=? AND Artiste=?');
+												$query5->execute(array($titre,$artiste));
+												$resultat=$query5->fetch();
+												$numMusique=$resultat['Numero_Musique'];
+												$query5->closeCursor();
+												
+												// on créée une instance dans la table "Uploader" qui lie les tables Utilisateur et musique
+												$query6=$bdd->prepare('INSERT INTO uploader(Identifiant, Numero_Musique) VALUES(:val1, :val2)');
+												$query6->execute(array('val1'=>$identifiant[0], 'val2'=>$numMusique));
+												echo('<p>Le fichier a bien été uploadé</p>');
+											}
 								}
 							}
-					}
+				    }
+				else{
+					echo("<p>Veuillez renseigner tous les champs</p>");
+				}
 			}
-			else{
-				echo "probleme";
-			}
-			
 		?>
 		</form>
-		<?php include("footer.php"); ?>
+			<?php /*include("footer.php");*/ ?>
  	</body>
  </html>
