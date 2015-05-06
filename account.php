9e837f1e276a14266f2c9947fcaf95bba18e0711
Cyril <cyril.yc.weller@gmail.com>
2015-05-06 14:15:17 +0200

    ajout infos page "mon compte"

--- account.php
+++ account.php
@@ -18,17 +18,54 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
    </head>
    	<body>
-		<?php include("header.php"); 
+
+		<?php include("header.php"); ?>
+
+
+   	<div>
+
+   	<form id="searchbar">
+		<?php
 		include("connect.php");
+
+		//Affichadge du pseudo
+		echo '<p> Votre pseudo : '.$identifiant[0].'</p>';
+
 		//Affichage de la date d'inscription 
 		$query=$bdd->prepare('SELECT Date_Inscription FROM utilisateur WHERE Identifiant = ?');
 		$query->execute(array($identifiant[0]));
 		$resultat = $query->fetch();
-		echo'<li> Inscrit le '.$resultat[0].'</li>';
+		echo'<p> Inscrit le '.$resultat[0].'</p>';
 
 		$query->closeCursor();
 
-	include("footer.php"); ?>
+		//Affichage du nombre de morceaux uploadés
+		$query=$bdd->prepare('SELECT Count(Titre) FROM uploader WHERE Identifiant = ?');
+		$query->execute(array($identifiant[0]));
+		$resultat = $query->fetch();
+		echo'<p> Nombre de morceaux uploadés: '.$resultat[0].'</p>';
+
+		$query->closeCursor();
+
+		//Affichage du nombre de playlist créées
+		$query=$bdd->prepare('SELECT Count(Numero_Playlist) FROM playlist WHERE Identifiant = ?');
+		$query->execute(array($identifiant[0]));
+		$resultat = $query->fetch();
+		echo'<p> Nombre de playlist créées: '.$resultat[0].'</p>';
+
+		$query->closeCursor();
+
+
+		echo '<form method="post" action="#">
+				<p><input name="Change_Password" value="Changer votre mot de passe" type="submit"></p>
+				<p><input name="Delete_Account" value="Supprimer votre compte" type="submit"></p>
+			</form>';
+
+		?>
+	</form>
+	</div>
+
+	<?php include("footer.php"); ?>
 	
  	</body>
  </html>
