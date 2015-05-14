bc822fc65df3281f31b23bd2ac80d4bbce966921
Aline - L <aline.legros80@gmail.com>
2015-05-14 15:51:48 +0200

    MAJ upload.php, upload.css
    prise en compte des espaces pour les artistes et titres
    -> impossible qu'il y ait à la fois dans la base :
    - shakaponk - runrunrun
    - shaka ponk  - run run run

--- upload.php
+++ upload.php
@@ -40,7 +40,7 @@
   							<option value="rb">R&amp;B</option>
 							<option value="reggae">Reggae</option>
   							<option value="rock">Rock</option>
-  							<option value="transcendantal">Transcendantal</option>
+  							<option value="transcendantale">Transcendantale</option>
 					</select>
 				</aside>
 			</div>
@@ -59,7 +59,6 @@
 										echo('<p>La taille du fichier ne doit pas dépasser 20 Mo!</p>');
 									}
 									else{
-											include("connect.php");
 											//on vérifie l'extension du fichier
 											$infosfichier = pathinfo($_FILES['fichier']['name']);
 											$type_file = $infosfichier['extension'];
@@ -67,13 +66,21 @@
 											if(!in_array($type_file,$ext_autorisees)){
 													echo('<p>Le fichier n\'est pas valide</p>');
 												}
-												else{	
+												else{
+														include("connect.php");
 														//On teste si la musique existe dans la table "musique" de la BD
 														$titre=strtolower($_POST['Titre']);
-														$artiste=strtolower($_POST['Artiste']);
+														$titreSansEspace=str_replace(' ','',$titre);
 														
-														$query3 = $bdd->prepare('SELECT COUNT(Numero_Musique) FROM musique WHERE Titre=? AND Artiste=?');
-														$query3->execute(array($titre,$artiste));
+														$artiste=strtolower($_POST['Artiste']);
+														$artisteSansEspace=str_replace(' ','',$artiste);
+														
+														$query3 = $bdd->prepare("SELECT COUNT(Numero_Musique)
+																				FROM musique
+																				WHERE REPLACE(Titre,' ','')=? 
+																				AND REPLACE(Artiste,' ','')=?"
+																				);
+														$query3->execute(array($titreSansEspace,$artisteSansEspace));
 														$resultat=$query3->fetch();
 														$nbMusiques = $resultat['COUNT(Numero_Musique)'];
 														$query3->closeCursor();
@@ -88,20 +95,28 @@
 															
 															// Et on ajoute une instance dans la base musique
 															$genre=strtolower($_POST['Style']);
-															$query4=$bdd->prepare('INSERT INTO musique(Artiste, Titre, Genre, Chemin_Musique) VALUES(:val1, :val2, :val3, :val4)');
+															$query4=$bdd->prepare('INSERT INTO musique(Artiste, Titre, Genre, Chemin_Musique) 
+																				   VALUES(:val1, :val2, :val3, :val4)'
+																				  );
 															$query4->execute(array('val1'=>$artiste, 'val2'=>$titre, 'val3'=>$genre, 'val4'=>$content_dir.$name_file));
 															
 														}
 														//Dans TOUS LES CAS :
 														// on récupère le numéro de la musique
-														$query5=$bdd->prepare('SELECT Numero_Musique FROM musique WHERE Titre=? AND Artiste=?');
-														$query5->execute(array($titre,$artiste));
+														$query5=$bdd->prepare("SELECT Numero_Musique 
+																			   FROM musique 
+																			   WHERE REPLACE(Titre,' ','')=? 
+																			   AND REPLACE(Artiste,' ','')=?"
+																			   );
+														$query5->execute(array($titreSansEspace,$artisteSansEspace));
 														$resultat=$query5->fetch();
 														$numMusique=$resultat['Numero_Musique'];
 														$query5->closeCursor();
 														
 														// on créée une instance dans la table "Uploader" qui lie les tables Utilisateur et musique
-														$query6=$bdd->prepare('INSERT INTO uploader(Identifiant, Numero_Musique) VALUES(:val1, :val2)');
+														$query6=$bdd->prepare('INSERT INTO uploader(Identifiant, Numero_Musique) 
+																			  VALUES(:val1, :val2)'
+																			  );
 														$query6->execute(array('val1'=>$identifiant[0], 'val2'=>$numMusique));
 														echo('<p>Le fichier a bien été uploadé</p>');
 													}
