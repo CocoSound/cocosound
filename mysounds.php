0d9660d7a89fdacfeca68f5935e06853967abce8
Ezerah <kilian.bellot@hotmail.fr>
2015-05-21 13:01:09 +0200

    Page mes sons
    Mise en place de la page
    Possibilite d'ajout des morceaux dans une des playlists
    Suppression des morceaux uploadés
    
    YOLO

--- mysounds.php
+++ mysounds.php
@@ -13,15 +13,24 @@
 <html>
    <head>
    	<meta charset='UTF-8'/>
-    	<title>Upload</title>
+    	<title>Mes musiques</title>
     	<meta name="viewport" content="width=device-width"/>
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		  <link rel="stylesheet" type="text/css" href="css/style.css" />
        		<link rel="stylesheet" type="text/css" href="css/search_results.css" />
    </head>
    	<body>
             <?php include("header.php"); ?>
             <?php
 				include("connect.php");
+                $query = $bdd->prepare('SELECT * FROM playlist WHERE identifiant = ?');
+        $query->execute(array($identifiant[0]));
+        $playlists = $query->fetchAll();
+        $select = "<select name='playlist_select'>";
+        foreach ($playlists as $playlist) {
+          $select .= "<option value='".$playlist['Numero_Playlist']."'>".$playlist['nom_playlist']."</option>";
+        }
+        $select .= "</select>";
+
 				$query = $bdd->prepare('SELECT * FROM musique,uploader WHERE uploader.identifiant = "'.$identifiant[0].'" 
                                         AND musique.numero_musique = uploader.numero_musique'); // requête SQL
 				$query->execute(); // paramètres et exécution
@@ -32,10 +41,21 @@
                                     <div class="title">'.$Numero_Musique['Titre'].'</div>
                                                     <div class="artist">- <b>'.$Numero_Musique['Artiste'].'</b></div>
                                                     <div class="style">'.$Numero_Musique['Genre'].'</div>
-                                                    <div><audio src="'.$Numero_Musique['Chemin_Musique'].'" controls></audio></div>
-                                                    <div class="sound-option">
+                                                    <div>
+                                                      <audio src="'.$Numero_Musique['Chemin_Musique'].'" controls></audio>
+                                                      <form class="addSongPlaylistForm" method="POST" action="addSongInPlaylist.php">
+                                                        <div class="playlist_div">'.$select.'</div>
+                                                        <input id="addPlaylist" type="submit" value="Ajouter"/>
+                                                        <input type="hidden" name="id_musique" value="'.$Numero_Musique['Numero_Musique'].'"/>
+                                                      </form>
+                                                      <form class="deleteSongForm" method="POST" action="deleteSound.php">
+                                                        <input type="hidden" name="idMusique" value="'.$Numero_Musique['Numero_Musique'].'"/>
+                                                        <div class="sound-option">
                                                             <button class="button">Supprimer</button>                            
-                                                    </div> 
+                                                        </div>
+                                                      </form> 
+                                                    </div>
+                                                    
                             </div>
                                             <div class="endline"></div>           
                         </div>
